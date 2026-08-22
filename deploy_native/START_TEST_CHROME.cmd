d870964b4f42df920523852304079ca4d7285109
ajyichen-pixel <aj.yichen@gmail.com>
2026-08-23 00:49:50 +0800

    Use Chrome for Testing for unpacked extension launch

--- deploy_native/START_TEST_CHROME.cmd
+++ deploy_native/START_TEST_CHROME.cmd
@@ -3,18 +3,24 @@ setlocal EnableExtensions
 set "ROOT=C:\Program Files\DVC\UploadGuard"
 set "EXT=%ROOT%\extension"
 set "PROFILE=%LOCALAPPDATA%\DVC\UploadGuardChromeProfile"
-set "CHROME="
+set "CHROME=%ProgramData%\DVC\UploadGuard\Browser\chrome-win64\chrome.exe"
 
-if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" set "CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-if not defined CHROME if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" set "CHROME=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
-if not defined CHROME if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" set "CHROME=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe"
+if not exist "%CHROME%" (
+  echo Chrome for Testing is required because current branded Chrome blocks --load-extension.
+  echo Downloading the official Google test browser now...
+  powershell -NoProfile -ExecutionPolicy Bypass -File "%ROOT%\GET_TEST_BROWSER.ps1"
+  if errorlevel 1 (
+    echo CHROME_FOR_TESTING_DOWNLOAD_FAILED
+    exit /b 2
+  )
+)
 
-if not defined CHROME (
-  echo CHROME_NOT_FOUND
-  exit /b 2
+if not exist "%CHROME%" (
+  echo CHROME_FOR_TESTING_NOT_FOUND
+  exit /b 3
 )
 
 if not exist "%PROFILE%" mkdir "%PROFILE%" >nul 2>&1
 start "DVC Upload Guard Test" "%CHROME%" --user-data-dir="%PROFILE%" --no-first-run --no-default-browser-check --disable-extensions-except="%EXT%" --load-extension="%EXT%" "chrome-extension://cdmogelilldmfcioieahdnaocmillhcl/test.html"
-echo CHROME_TEST_PROFILE_STARTED
+echo CHROME_FOR_TESTING_STARTED
 exit /b 0
