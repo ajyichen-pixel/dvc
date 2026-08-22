ae10de3765b00b662b75dadaa729e7b53a5e6b2d
ajyichen-pixel <aj.yichen@gmail.com>
2026-08-22 23:15:35 +0800

    Capture agent startup stdout stderr and exit code

--- deploy/START_AGENT.cmd
+++ deploy/START_AGENT.cmd
@@ -1,6 +1,14 @@
 @echo off
-setlocal
+setlocal EnableExtensions
 cd /d "%~dp0"
 if not exist "%ProgramData%\DVC\ContentAnalysis\logs" mkdir "%ProgramData%\DVC\ContentAnalysis\logs" >nul 2>nul
+set "BOOTLOG=%ProgramData%\DVC\ContentAnalysis\logs\agent_boot.log"
+echo ============================================================>>"%BOOTLOG%"
+echo START %DATE% %TIME%>>"%BOOTLOG%"
+echo CWD=%CD%>>"%BOOTLOG%"
+echo EXE=%~dp0DVCContentAnalysisAgent.exe>>"%BOOTLOG%"
 echo AGENT_START_REQUEST >> "%ProgramData%\DVC\ContentAnalysis\logs\dvc_content_analysis.log"
-"%~dp0DVCContentAnalysisAgent.exe"
+"%~dp0DVCContentAnalysisAgent.exe" >>"%BOOTLOG%" 2>&1
+set "RC=%ERRORLEVEL%"
+echo EXIT_CODE=%RC%>>"%BOOTLOG%"
+exit /b %RC%
